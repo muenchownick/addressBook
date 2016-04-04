a45fd5c93412267efd627d6b4c9d96a69243a89f
Inthrayuth Mahaphol and Nickolas Muenchow <inthrayuth@>
2016-04-04 11:07:09 -0700

    function correctly, saves all inputs and displays

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,40 @@
 // buisiness logic
-var Contact = function(first, last){
+var Contact = function(first, last, email){
+    this.firstName = first;
+    this.lastName = last;
+    this.eMail = email;
 
-}
+
+};
 
 
 
 // user interface logic
 $(document).ready(function(){
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+
+    var fName = $("#new-first-name").val();
+    var lName = $("#new-last-name").val();
+    var eMail = $("#new-email").val();
+    var newContact = new Contact(fName, lName, eMail);
 
 
+
+    $("#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+
+    $(".contact").last().click(function(){
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".email").text(newContact.eMail);
+
+    });
+
+    $("#new-first-name").val("");
+    $("#new-last-name").val("");
+    $("#new-email").val("");
+
+  });
 });
